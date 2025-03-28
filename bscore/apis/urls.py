3953ca84fdf3adbd09bdf5cda038ae2a1ef01826
princek <princesamuelpks@gmail>
2025-03-28 19:16:13 +0000

    Update urls.py

--- bscore/apis/urls.py
+++ bscore/apis/urls.py
@@ -3,5 +3,12 @@ import apis.views as views
 
 app_name = 'apis'
 urlpatterns = [
-    path('/', views.HealthCheckAPIView.as_view(), name='ping'),
+    path('', views.HealthCheckAPIView.as_view(), name='ping'),
+]
+
+# accounts and users
+urlpatterns += [
+    path('login/', views.LoginAPI.as_view(), name='login'),
+    path('verify-otp/', views.VerifyOTPAPI.as_view(), name='verify_otp'),
+    path('register/', views.RegisterAPI.as_view(), name='register_user'),
 ]
