a9656a7d278fa7690a0915e75877b8170d783a80
princek <princesamuelpks@gmail>
2025-03-28 17:04:34 +0000

    Create const.py

---
+++ bscore/bscore/utils/const.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class UserType(Enum):
+    '''User types for the application'''
+    CUSTOMER = 'CUSTOMER'
+    VENDOR = 'VENDOR'
+    ADMIN = 'ADMIN'
+    DELIVERY = 'DELIVERY'
+
+
+class ConstList:
+    '''Lists for the application'''
+    USER_TYPE = [
+        (user_type.value, user_type.value) for user_type in UserType
+    ]
+
+
+
+class SubscriptionType(Enum):
+    '''Subscription types for the application'''
+    PRODUCT = 'PRODUCT SELLER'
+    SERVICE = 'SERVICE PROVIDER'
+    ELITE = 'BUSINESS ELITE'
