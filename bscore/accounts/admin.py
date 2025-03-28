3c8a33eb4c3cb8858d02f425f99ded329cd6d03c
princek <princesamuelpks@gmail>
2025-03-28 19:32:18 +0000

    Update admin.py

--- bscore/accounts/admin.py
+++ bscore/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.site_header = 'BIRTHSALES PORTAL'
+
+# user
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'user_type','is_staff', 'is_superuser')
+    search_fields = ('name', 'email', 'phone',)
+
+# otp
+@admin.register(OTP)
+class OTPAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'otp', 'created_at', 'updated_at')
+    search_fields = ('phone', 'otp',)
+
