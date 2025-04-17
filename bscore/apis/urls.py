119c6a56bb397801622e9ed5e05b77d273fa4048
princek <princesamuelpks@gmail>
2025-04-17 23:34:52 +0000

    Update urls.py

--- bscore/apis/urls.py
+++ bscore/apis/urls.py
@@ -16,6 +16,7 @@ urlpatterns += [
     path('changepassword/', views.ChangePasswordAPIView.as_view(), name='change_password'), 
     path('resetpassword/', views.ResetPasswordAPIView.as_view(), name='reset_password'),
     path('users/', views.UsersAPIView.as_view(), name='users'),
+    path('services/', views.ServicesAPIView.as_view(), name='services'),
     path('vendors/', views.VendorsAPIView.as_view(), name='vendors'),
     path('vendorprofile/', views.VendorProfileAPIView.as_view(), name='vendorprofile'),
     path('subscriptionpackage/', views.SubscriptionPackageAPIView.as_view(), name='sub_package'),
