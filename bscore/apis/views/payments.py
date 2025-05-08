5d87aa0913a17c19bb904615ed2951956d474e1c
princek <princesamuelpks@gmail>
2025-05-08 02:05:46 +0000

    Update payments.py

--- bscore/apis/views/payments.py
+++ bscore/apis/views/payments.py
@@ -6,7 +6,9 @@ from accounts.models import Subscription, Vendor
 from apis.models import Payment
 from apis.serializers import PaymentSerializer
 from bscore.utils.const import PaymentType, UserType
-from bscore.utils.services import can_cashout, execute_momo_transaction
+from bscore.utils.permissions import allow_domains
+from bscore.utils.services import can_cashout, execute_momo_transaction, get_transaction_status
+from django.db.models import Q
 
 
 class PaymentAPIView(APIView):
@@ -70,22 +72,36 @@ class MakePaymentAPI(APIView):
     def post(self, request, *args, **kwargs):
         '''Make payment using mobile money'''
         subscription = request.data.get('subscription', None)
+        order = request.data.get('order', None)
+        booking = request.data.get('booking', None)
         vendor = None
         subscription = Subscription.objects.filter(id=subscription).first() if subscription else None
+        order = request.data.get('order', None) if order else None
+        booking = request.data.get('booking', None) if booking else None
+        print(f"subscription: {subscription}")
+        print(f"Vendor: {vendor}")
         if subscription:
-            vendor = Vendor.objects.filter(vendor_name__icontains='Birthon Account').first()
-            if not vendor:
+            vendor = Vendor.objects.filter(
+                Q(vendor_name__icontains='Birthnon Account') | 
+                Q(vendor_name__icontains='Birthnon Services'), 
+                Q(user__is_superuser=True)
+                ).first()
+            if vendor is None:
                 return Response({
                     "message": "Vendor not found",
                 }, status=status.HTTP_400_BAD_REQUEST)
         try:
             response = execute_momo_transaction(
                 request=request, 
-                type=PaymentType.DEBIT.value,
+                type=PaymentType.DEBIT.value, # debit the user and credit to the vendor
                 vendor=vendor,
+                subscription=subscription,
+                order=order,
+                booking=booking,
                 withdrawal=False,
                 )
         except Exception as e:
+            print(f"Exception caught: {e}")
             return Response({
                 "message": str(e),
             }, status=status.HTTP_400_BAD_REQUEST)
@@ -98,3 +114,38 @@ class MakePaymentAPI(APIView):
             status=response.get('api_status')
         )
 
+
+class PaymentCallbackAPI(APIView):
+    '''API Endpoint to handle payment callback'''
+    permission_classes = [permissions.AllowAny]
+
+    @allow_domains(['localhost:8000'],)
+    def get(self, request, *args, **kwargs):
+        '''Handle payment callback'''
+        return Response({
+            "message": "Callback received",
+        }, status=status.HTTP_200_OK)
+    
+class PaymentStatusCheckAPI(APIView):
+    '''API Endpoint to check payment status'''
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        '''Check payment status'''
+        payment_id = request.query_params.get('payment_id', None)
+        if not payment_id:
+            return Response({
+                "message": "Payment ID is required",
+            }, status=status.HTTP_400_BAD_REQUEST)
+        payment = Payment.objects.filter(payment_id=payment_id).first()
+        if not payment:
+            return Response({
+                "message": "Payment not found",
+            }, status=status.HTTP_404_NOT_FOUND)
+        # check if payment is successful
+        response = get_transaction_status(payment.payment_id)
+        payment.status_code = response.get('status_code')
+        payment.status = response.get('message')
+        payment.save()
+        serializer = PaymentSerializer(payment)
+        return Response(serializer.data, status=status.HTTP_200_OK)
